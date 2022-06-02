231b61ee909a5d90ada764f571b0da0dd363c649
Jordan <lav.jordan@live.com>
2022-06-01 18:22:19 -0700

    add btns to play rounds

--- java.js
+++ java.js
@@ -1,7 +1,28 @@
+// Logs button clicked
+const btns = document.querySelectorAll(".btn");
+btns.forEach((button) => {
+    button.addEventListener('click', playerPlay);
+});
+
+
+
+// vvv GAME FUNCTIONS vvv
+
+// set rounds won to 0
+let playerscore = 0;
+let computerscore = 0;
+
+// get user input function
+let player;
+function playerPlay(e) {
+    if (playerscore > 4 || computerscore > 4) {return;} //check if game over
+    player = (e.target.id);
+    playRound (player, computerPlay());
+}
+
 // get computer input function
     // randomnumber variable = random number between 1 and 3
     // Assign rock, paper or scissors to computer variable with if statements. 1 = rock. 2 = paper. 3 = scissors.
-    // return computer variable.
 function computerPlay() {
     let randomInt = Math.floor(Math.random() * (4-1) + 1);
     let computer;
@@ -17,42 +38,19 @@ function computerPlay() {
     return computer;
 }
 
-// get user input function
-    // convert to all lowercase
-    // if not equal to rock, paper or scissors, give error message and ask again
-    // return players choice
-
-function playerPlay() {
-    let player = window.prompt("Rock, paper or scissors?").toLowerCase();
-    if (player == "rock" || player == "paper" || player == "scissors") {
-        return player;
-    }
-    else {
-        console.log(`You entered ${player}, please enter rock, paper or scissors`);
-        playerPlay();
-    }
-}
-
-// Temp remove BO5 rounds function
-
-// set rounds won to 0
-let playerscore = 0;
-let computerscore = 0;
-
 // create iefe function game that will keep playing rounds until one player wins 3 rounds(best 3 out of 5)
-(function game() {
-    console.log("Game started\n\n\n")
-    let i = 0;
-    while(playerscore < 5 && computerscore < 5) {
-        i++;
-        console.log(`Player: ${playerscore} Computer: ${computerscore} Round: ${i}`);
-        // get user and computer input;
-        let player = playerPlay();
-        let computer = computerPlay();
-        // call playRound function  
-        playRound(player, computer);
-    }
-})()
+// function game(player) {
+//     console.log("Game started\n\n\n")
+//     let i = 0;
+//     while(playerscore < 5 && computerscore < 5) {
+//         i++; //round tracker
+//         console.log(`Player: ${playerscore} Computer: ${computerscore} Round: ${i}`);
+//         // get computer input;
+//         let computer = computerPlay();
+//         // call playRound function  
+//         playRound(player, computer);
+//     }
+// }
 
 // create function to play single round of rock, paper, scissors
     //compare player input to each possible computer input
@@ -97,6 +95,13 @@ function playRound(player, computer) {
                 break;
             }
     }   
+    // Show overall winner 
+    if (playerscore > 4) {
+        console.log(`You won! ${playerscore} to ${computerscore}`);
+    }
+    else if (computerscore > 4) {
+        console.log(`You lost! ${playerscore} to ${computerscore}`)
+    }
 }
 
 //display winner and increase winners score by 1
@@ -113,14 +118,4 @@ function lose(player, computer) {
 
 function tie(player, computer) {
     console.log(`It's a tie! You both chose ${player}\n\n`);
-}
-
-// create win statement if playerscore = 3
-// create lose statement if computerscore = 3
-
-if (playerscore == 5) {
-    console.log(`You won! ${playerscore} to ${computerscore}`);
-}
-else if (computerscore == 5) {
-    console.log(`You lost! ${playerscore} to ${computerscore}`)
 }
